6fa13e210b3ff7c6bf6aa2d90ed4c487b57bbc58
cornerobscurial <corner@obscurial.net>
2025-08-19 18:12:07 +0300

    Added type checking and string start end support to BaseFeature

--- feature.py
+++ feature.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import pandas as pd
+from typing import List, Union
+from pydantic import BaseModel, validator
 
 class BaseFeature(ABC):
     def __init__(self, *args, **kwargs):
@@ -9,6 +12,12 @@ class BaseFeature(ABC):
     def calculate(self):
         pass
     
-    def get_feature(self, start, end, pairs):
+    def get_feature(self, start: Union[str, pd.Timestamp], end: Union[str, pd.Timestamp], pairs: List[str]):
+        # Convert string inputs to pd.Timestamp if needed
+        if isinstance(start, str):
+            start = pd.Timestamp(start)
+        if isinstance(end, str):
+            end = pd.Timestamp(end)
+        
         output = self.calculate(start, end, pairs, *self.args, **self.kwargs)
         return output
