5e901b4ac3cadd938eac5318fe0bba58e80ac6ae
cornerobscurial <corner@obscurial.net>
2025-08-19 17:09:13 +0300

    Initial feature definition

---
+++ feature.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Feature(ABC):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+    
+    @abstractmethod
+    def calculate(self):
+        pass
+    
+    def get_feature(self, start, end, pairs):
+        output = self.calculate(start, end, pairs, *self.args, **self.kwargs)
+        return output
