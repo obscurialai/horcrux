a1c04c7a9acd83fd526d6a35207aae053c6fb7b5
cornerobscurial <corner@obscurial.net>
2025-08-19 18:26:26 +0300

    Added handling for str values of pairs for BaseFeature

--- feature.py
+++ feature.py
@@ -12,12 +12,15 @@ class BaseFeature(ABC):
     def calculate(self):
         pass
     
-    def get_feature(self, start: Union[str, pd.Timestamp], end: Union[str, pd.Timestamp], pairs: List[str]):
+    def get_feature(self, start: Union[str, pd.Timestamp], end: Union[str, pd.Timestamp], pairs: Union[str, List[str]]):
         # Convert string inputs to pd.Timestamp if needed
         if isinstance(start, str):
             start = pd.Timestamp(start)
         if isinstance(end, str):
             end = pd.Timestamp(end)
+        # Convert string pairs to list if needed
+        if isinstance(pairs, str):
+            pairs = [pairs]
         
         output = self.calculate(start, end, pairs, *self.args, **self.kwargs)
         return output
