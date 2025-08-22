ce4f1869350caf7de356dcf56d1d39f16e04f9a6
cornerobscurial <corner@obscurial.net>
2025-08-22 15:43:39 +0300

    Added FUnion to __init__

--- src/horcrux/__init__.py
+++ src/horcrux/__init__.py
@@ -2,5 +2,6 @@ from .ohlcv import OHLCV
 from .feature import Feature
 from .zscore import ZScore
 from .rolling_linreg_slope import RollingLinRegSlope
+from .feature_union import FUnion
 
-__all__ = ['OHLCV', 'Feature', 'ZScore', 'RollingLinRegSlope'] 
+__all__ = ['OHLCV', 'Feature', 'ZScore', 'RollingLinRegSlope', 'FUnion'] 
